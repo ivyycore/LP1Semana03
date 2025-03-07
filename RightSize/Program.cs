906d3606674acd1de9b9ee97495fd1aec01e76ec
ivyycore <mariasequeira.dev@gmail.com>
2025-03-07 04:10:42 +0000

    Fixed errors and finished code

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -8,11 +8,16 @@ namespace RightSize
         {
             foreach (string s in args)
             {
-                if (s.length>7)
+                if (s.Length>7)
                 {
                     Console.WriteLine("[EARLY STOP]");
                     break;
                 }
+                else if (s.Length>3)
+                {
+                    Console.WriteLine(s);
+                }
+            }
         }
     }
 }
