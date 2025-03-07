2b2f37f6c8a83bd0c723d0628341d76314142c03
ivyycore <mariasequeira.dev@gmail.com>
2025-03-07 04:04:37 +0000

    Added foreach and first if

---
+++ RightSize/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string s in args)
+            {
+                if (s.length>7)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+        }
+    }
+}
