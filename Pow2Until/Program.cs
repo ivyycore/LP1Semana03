8bbab8d475491791095cfde5267cddcf9924926b
ivyycore <mariasequeira.dev@gmail.com>
2025-03-07 13:40:32 +0000

    Added method

--- Pow2Until/Program.cs
+++ Pow2Until/Program.cs
@@ -6,7 +6,13 @@ namespace Pow2Until
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+        private static void PowersOf2Until5 ( )
+        {
+        for (int i = 1; i <= ( 1 << 5 ); i = i << 1)
+            {
+                Console.WriteLine ( i ) ;
+            }
+        }
         }
     }
 }
