9347a688c8478728202b57252354e3463ffaf689
ivyycore <mariasequeira.dev@gmail.com>
2025-03-07 10:32:25 +0000

    Took care of the array and switch condition

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -6,8 +6,30 @@ namespace HeroPerk
     {
         private static void Main(string[] args)
         {
-                
-        }
+            string selectedPerk = args[0].ToLower(); 
+            int[] count = new int [4]; //creates array
+
+            foreach (char c in selectedPerk) //for each char in args
+            {
+                switch(c)
+                {
+                    case 'warpshift': 
+                        count[0]++;
+                        break;
+                    case 'stealth': 
+                        count[1]++;
+                        break;
+                    case 'autoheal': 
+                        count[2]++;
+                        break;
+                    case 'doublejump': 
+                        count[3]++;
+                        break;
+                    default: 
+                        Console.WriteLine($"!Unknown perk!");
+                        return;
+                }
+            }
     
     [Flags]
     enum Perks
