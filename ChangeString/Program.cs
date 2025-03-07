b6b8a65aa69f787ecd00b2e617104415d7f02158
ivyycore <mariasequeira.dev@gmail.com>
2025-03-07 04:16:55 +0000

    Made input system

---
+++ ChangeString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Tell me something: "); //ask for input
+            string frase = Console.ReadLine(); // turn input into string
+            Console.Write("Write a character/letter: ") // ask for input
+            string letra = Console.Readline(); // turn input intro string 
+        }
+    }
+}
