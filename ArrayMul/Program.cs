5c01ed3ef71b06de5e6c5cb66e1465aeb20bffa1
ivyycore <mariasequeira.dev@gmail.com>
2025-03-07 04:32:47 +0000

    Created the matrixes

---
+++ ArrayMul/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArrayMul
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+            float[,] Mb = new float [2,2];
+
+            float[] Ma = new float [2];
+        }
+    }
+}
