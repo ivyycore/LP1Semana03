cbb5606b85b247e5325e8d61f11d9c02872a36bd
ivyycore <mariasequeira.dev@gmail.com>
2025-03-07 04:35:03 +0000

    Defined matrixes

--- ArrayMul/Program.cs
+++ ArrayMul/Program.cs
@@ -8,9 +8,19 @@ namespace ArrayMul
         {
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 
-            float[,] Mb = new float [2,2];
+            float[,] Mb = new float [2,2]; // creates a matrix
 
-            float[] Ma = new float [2];
+            float[] Ma = new float [2]; // creates a matrix 
+
+            Ma[0,0] = float.Parse(args[0]);
+            Ma[0,1] = float.Parse(args[1]);
+            Ma[1,0] = float.Parse(args[2]);
+            Ma[1,1] = float.Parse(args[3]);
+            Mb[0] = float.Parse(args[4]);
+            Mb[1] = float.Parse(args[5]);
+
+
+            
         }
     }
 }
