f16298aa19bdd6e78c05db6c4e7e4b4882ab883b
ivyycore <mariasequeira.dev@gmail.com>
2025-03-07 15:13:41 +0000

    Finished the exercises

---
+++ RoPaSci/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace RoPaSci
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            GameItem player1 = Enum.Parse<GameItem>(args[0]);
+            GameItem player2 = Enum.Parse<GameItem>(args[1]);
+            
+            GameStatus result = RockPaperScissors(player1, player2);
+
+            switch (result)
+            {
+                case GameStatus.Draw:
+                    Console.WriteLine("It's a draw!");
+                    break;
+                case GameStatus.Player1Wins:
+                    Console.WriteLine("Player 1 wins!");
+                    break;
+                case GameStatus.Player2Wins:
+                    Console.WriteLine("Player 2 wins!");
+                    break;
+            }
+        }
+
+            private static  GameStatus RockPaperScissors(GameItem player1, GameItem player2)
+            {
+                if (player1 == player2)
+                {
+                    return GameStatus.Draw; 
+                }
+                if (((player1 == GameItem.Rock) && (player2 == GameItem.Scissors)) ||
+                    ((player1 == GameItem.Scissors) && (player2 == GameItem.Paper)) ||
+                    ((player1 == GameItem.Paper) && (player2 == GameItem.Rock)))
+                {
+                    return GameStatus.Player1Wins; 
+                }
+                else
+                {
+                    return GameStatus.Player2Wins; 
+                }
+            }
+    }
+    enum GameItem
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+
+    enum GameStatus
+    {
+        Draw,
+        Player1Wins,
+        Player2Wins
+    }
+}
