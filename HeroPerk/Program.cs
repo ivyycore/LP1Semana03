d5b7ca990124cfd521814914c45a4ccb33bb6f90
ivyycore <mariasequeira.dev@gmail.com>
2025-03-07 10:20:30 +0000

    Added enumeration flags

---
+++ HeroPerk/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HeroPerk
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+                
+        }
+    
+    [Flags]
+    enum Perks
+    {
+        WarpShift = 1 << 0,
+        AutoHeal = 1 << 1,
+        Stealth = 1 << 2,
+        DoubleJump = 1 << 3
+    }
+}
