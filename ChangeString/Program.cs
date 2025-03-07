8ca4115d13ec1bda537bf73e5dc22e7cc638d316
ivyycore <mariasequeira.dev@gmail.com>
2025-03-07 04:22:44 +0000

    Did new phrase system and fixed stuff

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -8,10 +8,13 @@ namespace ChangeString
         {
             Console.Write("Tell me something: "); //ask for input
             string frase = Console.ReadLine(); // turn input into string
-            Console.Write("Write a character/letter: ") // ask for input
-            string letra = Console.Readline(); // turn input intro string
+            Console.Write("Write a character/letter: "); // ask for input
+            string letra = Console.ReadLine(); // turn input intro string
 
             char letra_to_char = char.Parse(letra); // turns letra into char 
+
+            string novafrase = frase.Replace(letra_to_char,'x');
+            Console.WriteLine(novafrase);
         }
     }
 }
